c7357c4e1be424cfcd8643fb9fdfdc42c2d5da9f
ivanromero07 <ia.romerom@uniandes.edu.co>
2026-05-12 09:29:38 -0500

    Multiply cupcakes by 10
    We scaled up to a factory

--- code/cupcake_code.R
+++ code/cupcake_code.R
@@ -9,8 +9,8 @@ make_cupcake <- function(n_cupcakes = 1) {
   ingredients <- c("flour", "sugar", "milk", "chocolate")
 
   cat("Mixing:", paste(ingredients, collapse = ", "), "\n")
-  cupcakes <- strrep("\U0001F9C1", n_cupcakes)
+  cupcakes <- strrep("\U0001F9C1", 10*n_cupcakes)
   print(paste0("Cupcakes complete! ", cupcakes))
 }
 
-make_cupcake(n_cupcakes = 5)
+make_cupcake(n_cupcakes = 21)
